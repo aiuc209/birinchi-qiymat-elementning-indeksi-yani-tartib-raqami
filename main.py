e792f782225d11aacf345810be567aaa4ec1ac96
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:27:51 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Birinchi Qiymat Elementning Indeksi Yani Tartib Raqami
+Topic: * birinchi qiymat — elementning indeksi (ya’ni tartib raqami),
+"""
+
+def main():
+    print("Welcome to Birinchi Qiymat Elementning Indeksi Yani Tartib Raqami!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
